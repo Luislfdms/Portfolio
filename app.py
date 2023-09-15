dfef3f8189907a876b163da2fe8919c1d8ce0baa
Luislfdms <luislfdms18@gmail.com>
2023-09-14 21:06:47 -0400

    include port and host within the main py file

--- app.py
+++ app.py
@@ -9,5 +9,7 @@ app.register_blueprint(projects.projects_bp)
 app.register_blueprint(about.about_bp)
 app.register_blueprint(experience.experience_bp)
 
+port = int(os.environ.get('PORT', 10000))
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)

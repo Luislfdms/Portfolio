90d01d5010fa697e3aba81f95d6599ef9055af0b
Luislfdms <luislfdms18@gmail.com>
2023-09-14 20:44:14 -0400

    removed images for now as they weren't working properly and added new project

--- pages/projects.py
+++ pages/projects.py
@@ -9,12 +9,16 @@ def projects():
                       of members of the channel or anybody else in general if they have their username. With initial setup you can link \
                       your league of legends username to your discord which allows users to lookup your stats using your discord name in the \
                       case that they dont know your usename. The names are saved in a mysql database and the bot is ran on an AWS instance.'], 
-                     ['Finance Tracker','Example Description'], 
+                     ['This Website!','I have been wanting to create a website incorporating flask for a while and then I realized well I still \
+                       not have a portfolio site! Overall This site was really fun to make however I did run into a couple issues such as the home \
+                       page which took me a while to get right but in the end I believe it was well worth it. I will be hosting it on render \
+                       as it has great support for flask applications and I already have experience with google cloud and AWS already so I want to try something new.'], 
                      ['Travel Pal','Example Description']]
-    technology = [['Python','AWS','Riot API', 'discord.py','selenium','mysql']]
+    technology = [['Python','AWS','Riot API', 'discord.py','selenium','mysql'],
+                  ['Python', 'HTML/CSS', 'JavaScript', 'Flask', 'Render', 'Web Development']]
 
     # Render the "projects.html" template with the provided data
-    return render_template('projects.html', projects_list=projects_list)
+    return render_template('projects.html', projects_list=projects_list, technology=technology)
 
 @projects_bp.route('/projects')
 def technology_list(elements):

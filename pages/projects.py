8363a4bbc80d26a87cc0a5bda476c888ee7cc761
Luislfdms <luislfdms18@gmail.com>
2023-09-18 00:23:53 -0400

    added resume to about and added new project

--- pages/projects.py
+++ pages/projects.py
@@ -13,9 +13,15 @@ def projects():
                        not have a portfolio site! Overall This site was really fun to make however I did run into a couple issues such as the home \
                        page which took me a while to get right but in the end I believe it was well worth it. I will be hosting it on render \
                        as it has great support for flask applications and I already have experience with google cloud and AWS already so I want to try something new.'], 
-                     ['Travel Pal','Example Description']]
+                     ['Finance Express','Although this is still in progress as of now I want to include as my other projects I feel are rather small to \
+                       showcase here. Finance Express is a finance tracking application im working on untilizing react native and the plaid api. I am planning \
+                       on working on this app in multiple stages, first I want users to be able to track their expenses and have them placed in categories, \
+                       and be able to create budgets. After these basics of a finance tracking app are working I want to encorporate A.I using openai api. \
+                       The added functionality I want from the A.I is a smart tool that helps you find areas where you can be saving or maybe are being overcharged \
+                       compared to other users.']]
     technology = [['Python','AWS','Riot API', 'discord.py','selenium','mysql'],
-                  ['Python', 'HTML/CSS', 'JavaScript', 'Flask', 'Render', 'Web Development']]
+                  ['Python', 'HTML/CSS', 'JavaScript', 'Flask', 'Render', 'Web Development'],
+                  ['React Native and Navigation', 'TypesScript', 'Node.js', 'MongoDB', 'Plaid API', 'Github']]
 
     # Render the "projects.html" template with the provided data
     return render_template('projects.html', projects_list=projects_list, technology=technology)

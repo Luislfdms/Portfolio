e8caa52da8ff9d239f8b122e4e478f29483f19f4
Luislfdms <luislfdms18@gmail.com>
2025-03-05 16:17:50 -0500

    Add new experience and fix container for mobile vi ew on projects

--- pages/projects.py
+++ pages/projects.py
@@ -5,7 +5,7 @@ from . import projects_bp
 @projects_bp.route('/')
 def projects():
     # Define any variables you want to pass to the template
-    projects_list = [['League Stats', 'Discord bot that can be added to a channel and allows users to lookup legue of legends stats \
+    projects_list = [['League Stats', 'Discord bot that can be added to a channel and allows users to lookup users League of Legends stats \
                       of members of the channel or anybody else in general if they have their username. With initial setup you can link \
                       your league of legends username to your discord which allows users to lookup your stats using your discord name in the \
                       case that they dont know your usename. The names are saved in a mysql database and the bot is ran on an AWS instance.'], 
